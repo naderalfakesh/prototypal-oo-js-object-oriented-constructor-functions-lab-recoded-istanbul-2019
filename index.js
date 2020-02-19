06200ab51bf0517426fdad100629634a7d8904ac
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 09:14:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function Scooter(year,color,model){
   this.year=year;
   this.model=model;
 }
+
+function Scooter(year,color,model){
+  this.color=color;
+  this.year=year;
+  this.model=model;
+}
+
+function Scooter(year,color,model){
+  this.color=color;
+  this.year=year;
+  this.model=model;
+}
