6240a0ceb446fbd711f2e71df6ee2c3c6ecf6a7f
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 09:15:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year,color,model){
+  this.color=color;
+  this.year=year;
+  this.model=model;
+}
+
+function Driver(name,age,experience){
+  this.name=name;
+  this.age=age;
+  this.experience=experience;
+}
+
+function PickupLocation(address,city){
+  this.address=address;
+  this.city=city;
+}
