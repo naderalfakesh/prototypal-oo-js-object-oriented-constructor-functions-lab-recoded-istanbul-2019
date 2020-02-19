a7b4ef9f19fc3f7ae5beeb607f66e43940310ae2
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 09:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function Driver(name,age,experience){
   this.experience=experience;
 }
 
-function Scooter(year,color,model){
+function PickupLocation(year,color,model){
   this.color=color;
   this.year=year;
   this.model=model;
